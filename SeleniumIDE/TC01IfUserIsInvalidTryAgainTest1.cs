fa9f0e5955df5028e076447a2fe796ea31eea361
denevadianadeneva-star <denevadianadeneva@gmail.com>
2026-08-12 20:46:47 +0300

    Fix ChromeDriver options for headless execution

--- SeleniumIDE/TC01IfUserIsInvalidTryAgainTest1.cs
+++ SeleniumIDE/TC01IfUserIsInvalidTryAgainTest1.cs
@@ -21,7 +21,13 @@ public class TC01IfUserIsInvalidTryAgainTest
     [SetUp]
     public void SetUp()
     {
-        driver = new ChromeDriver();
+        ChromeOptions option = new ChromeOptions();
+        option.AddArguments("headless");
+        option.AddArguments("no-sandbox");
+        option.AddArguments("disable-dev-shm-usage");
+        option.AddArguments("disable-gpu");
+        option.AddArguments("window-size=1920x1080");
+        driver = new ChromeDriver(option);
         js = (IJavaScriptExecutor)driver;
         vars = new Dictionary<string, object>();
     }
